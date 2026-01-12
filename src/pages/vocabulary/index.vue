8528890fb54026fe88498a6775d37682be0cb98c
7pm <143283299+cc77kx@users.noreply.github.com>
2026-01-12 15:26:29 +0800

    Fix table class syntax error - add quotes to class attribute

--- src/pages/vocabulary/index.vue
+++ src/pages/vocabulary/index.vue
@@ -258,8 +258,7 @@ function copyAllError() {
         <div class="overflow-x-auto rounded-lg">
           <div class="inline-block min-w-full align-middle">
             <div class="overflow-hidden shadow sm:rounded-lg">
-              <table class=hidden md:table "min-w-full divide-y divide-gray-200 dark:divide-gray-600">
-                <thead class="bg-gray-50 dark:bg-gray-700">
+              <table class="hidden md:table min-w-full divide-y divide-gray-200 dark:divide-gray-600">                <thead class="bg-gray-50 dark:bg-gray-700">
                   <tr>
                     <th class="p-4 text-left text-xs font-medium tracking-wider text-gray-500 dark:text-white">
                     </th>
